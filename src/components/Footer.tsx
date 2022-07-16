0f8abbca83c4a09887d36378abde5ecb1e298bb6
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2022-07-16 23:09:48 +0900

    ✨ フッターコンポーネントを新規作成

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+export const Footer = () => {
+  return (
+    <footer className="mt-20">
+      <div className=" bg-zinc-300 py-10 text-center">
+        <h4 className="inline-block border-b-2 border-black pb-4 text-3xl">
+          About
+        </h4>
+        <p className="mt-10">
+          "Penneko IT Blog"
+          は、Webフロントエンド開発に関するトピックを発信する技術ブログです。
+        </p>
+        <p className="mt-5">
+          このサイトはmicroCMSで構築しNetlifyから配信しています。
+        </p>
+        <p className="mt-5">プライバシーポリシー</p>
+      </div>
+      <div className="h-16 bg-black p-5 text-center text-white">
+        &copy;copyright
+      </div>
+    </footer>
+  );
+};
