dfcad72fedbb08e456b9ae3f414d679ce8873f93
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2022-07-24 17:53:09 +0900

    💄 CDNで目次スタイルを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,18 @@ import type { AppProps } from "next/app";
 import { Header } from "src/components/Common/Header";
 import { CommonWrapper } from "src/components/Common/CommonWrapper";
 import { Footer } from "src/components/Common/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Penneko IT Blog</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.11.1/tocbot.css"
+        />
+      </Head>
       <Header />
       <main>
         <CommonWrapper>
