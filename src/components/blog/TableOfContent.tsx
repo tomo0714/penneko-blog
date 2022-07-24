8956ae907770639d00d6c3520274652089229e7b
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2022-07-24 17:52:45 +0900

    🔥 不要な記述を削除

--- src/components/blog/TableOfContent.tsx
+++ src/components/blog/TableOfContent.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
-import { useEffect, VFC } from "react";
-import tocbot from "tocbot";
+import { VFC } from "react";
 
 /** 目次表示用コンポーネント */
-export const TableOfContent: VFC = (props) => {
+export const TableOfContent: VFC = () => {
   return (
     <aside className="sticky top-10 h-[27rem] w-96 shadow-lg">
       <h2 className="flex h-16 items-center p-6 shadow-lg">目次</h2>
