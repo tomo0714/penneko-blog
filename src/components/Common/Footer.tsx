9ae744ad05f5f86e685d6c26c066d9f10b76d273
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2022-07-31 15:33:42 +0900

    ⚡️ Aboutの文言を修正

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -8,10 +8,11 @@ export const Footer = () => {
         </h4>
         <p className="mt-10 px-5">
           &quot;Penneko IT Blog&quot;
-          は、Webフロントエンド開発に関するトピックを発信する技術ブログです。
+          は、Web技術に関するトピックスを配信するブログサイトです。
         </p>
         <p className="mt-5 px-5">
-          このサイトはmicroCMSで構築しNetlifyから配信しています。
+          このサイトはNext.js + Typescript +
+          microCMSで構築しNetlifyから配信しています。
         </p>
       </div>
       <div className="h-16 bg-black p-5 text-center text-white">
